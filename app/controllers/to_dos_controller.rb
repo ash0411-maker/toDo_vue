0a2493b2471bd756089b3b9c887446cfdd9ea907
ash <uetani.0411@gmail.com>
2020-09-22 09:55:26 +0900

    [add] new action

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,5 +1,9 @@
 class ToDosController < ApplicationController
-    def index
-        @to_dos = ToDo.all
-    end
+  def index
+    @to_dos = ToDo.all
+  end
+
+  def new
+    @to_do = ToDo.new
+  end
 end
