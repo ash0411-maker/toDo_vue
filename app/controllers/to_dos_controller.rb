85004e8f6534a075e07d82892c9bcffc81352219
ash <uetani.0411@gmail.com>
2020-09-22 09:58:11 +0900

    [add] create action

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -6,4 +6,13 @@ class ToDosController < ApplicationController
   def new
     @to_do = ToDo.new
   end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to to_dos_url
+    else
+      render :new
+    end
+ end
 end
