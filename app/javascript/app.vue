899ba9165157c80783df36cc1be336a7d0d12e2b
ash <uetani.0411@gmail.com>
2020-09-23 13:17:19 +0900

    [add] Element UIのボタンを表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <el-button>Default</el-button>
   </div>
 </template>
 
@@ -8,7 +9,7 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "やっとできた！！！！"
     }
   }
 }
